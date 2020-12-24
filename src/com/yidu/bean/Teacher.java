6132c4e139a129233b2554db299ae475c623888b
xiaoyiluo233 <76541540+xiaoyiluo233@users.noreply.github.com>
2020-12-24 15:55:14 +0800

    Update Teacher.java

--- src/com/yidu/bean/Teacher.java
+++ src/com/yidu/bean/Teacher.java
@@ -7,6 +7,7 @@ package com.yidu.bean;
  * @vesion:1.0
  */
 public class Teacher {
+    private String name="李小龙";
     public static void main(String[] args) {
         System.out.println("第二次的提交");
     }
