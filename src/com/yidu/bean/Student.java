17479f595fdf72e07da71248971a517f7b891d25
xiaoyiluo233 <2690291311@qq.com>
2020-12-24 15:08:56 +0800

    这是我的第一次提交

---
+++ src/com/yidu/bean/Student.java
@@ -0,0 +1,14 @@
+package com.yidu.bean;
+
+/**
+ * @description:
+ * @author:lixiang
+ * @date: 2020/12/24 14:52
+ * @vesion:1.0
+ */
+public class Student {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
